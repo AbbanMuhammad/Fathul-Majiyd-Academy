0020da87f9ddf30a2fa88701ea7cdc233bf99995
Sani Ismail <saniismail2@gmail.com>
2025-08-26 19:29:05 +0100

    add some interactivities in the page.

---
+++ script.js
@@ -0,0 +1,9 @@
+// Responsive Navbar Toggle
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const nav = document.querySelector(".header nav");
+
+  menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+});
